96791272691695600a5751f40a85b9e3a5c96413
MehmetBilirr <mehmetbilir@aol.com>
2023-11-25 16:45:24 +0300

    Initial Commit

---
+++ InnovaCase/Screens/MainViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MainViewController.swift
+//  InnovaCase
+//
+//  Created by Mehmet Bilir on 25.11.2023.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+  @IBOutlet weak var tableView: UITableView!
+  override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+
+}
